e6072b4b59887346011286d5a1e9be53ab1d97b8
Pavel <panteleevofficial@yandex.ru>
2021-10-02 23:49:33 +0300

    lesson 6 in progress

--- app/src/main/java/com/example/weatherapp_v2/ui/main/model/Weather.kt
+++ app/src/main/java/com/example/weatherapp_v2/ui/main/model/Weather.kt
@@ -1,30 +1,7 @@
 package com.example.weatherapp_v2.ui.main.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class Weather(var temperature: Int = 0, var feelsLike: Int = 0) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readInt()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(temperature)
-        parcel.writeInt(feelsLike)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Weather> {
-        override fun createFromParcel(parcel: Parcel): Weather {
-            return Weather(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Weather?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class Weather(var temperature: Int = 0, var feelsLike: Int = 0) : Parcelable
--- app/src/main/java/com/example/weatherapp_v2/ui/main/model/WeatherDTO.kt
+++ app/src/main/java/com/example/weatherapp_v2/ui/main/model/WeatherDTO.kt
@@ -1,11 +1,15 @@
 package com.example.weatherapp_v2.ui.main.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class WeatherDTO(
     val fact: FactDTO?
 ) {
+    @Parcelize
     data class FactDTO(
         val temp: Int?,
         val feels_like: Int?,
         val condition: String?
-    )
+    ) : Parcelable
 }
